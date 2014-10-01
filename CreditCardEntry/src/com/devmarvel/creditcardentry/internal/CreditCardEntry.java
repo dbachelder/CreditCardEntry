004c5322d1e4191b8c10083845efce64bc8695b6
Evyatar Segal <evyatar@ridewithvia.com>
2014-10-01 12:12:58 -0400

    Revert "Disable the CC widget's excessive sliding."
    This reverts commit 9af91e1a0dab0cc4f8d9c49da948f8b64c137873.

--- CreditCardEntry/src/com/devmarvel/creditcardentry/internal/CreditCardEntry.java
+++ CreditCardEntry/src/com/devmarvel/creditcardentry/internal/CreditCardEntry.java
@@ -237,11 +237,20 @@ public class CreditCardEntry extends HorizontalScrollView implements
 			this.textHelper.setText(field.helperText());
 		}
 
-        // NOTE(evyatar): the logic ahead is a weird hacky. it was here in the original package,
-        // and I was unable to make the widget look good without it. If we keep the timer's
-        // numbers small, the sliding effect will remain un-noticable.
-		if (! field.getClass().equals(CreditCardText.class)) {
-			new CountDownTimer(100, 10) {
+		if (field.getClass().equals(CreditCardText.class)) {
+			new CountDownTimer(1000, 20) {
+
+				public void onTick(long millisUntilFinished) {
+					CreditCardEntry.this.scrollTo((int) (millisUntilFinished),
+							0);
+				}
+
+				public void onFinish() {
+					CreditCardEntry.this.scrollTo(0, 0);
+				}
+			}.start();
+		} else {
+			new CountDownTimer(1500, 20) {
 
 				public void onTick(long millisUntilFinished) {
 					CreditCardEntry.this.scrollTo(
