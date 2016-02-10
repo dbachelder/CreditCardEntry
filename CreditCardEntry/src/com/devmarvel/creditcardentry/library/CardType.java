aad31377b08169640ba50a75175e1c040ba3d46e
StephenVinouze <s.vinouze@gmail.com>
2016-02-10 14:25:43 +0100

    Update JCB logo and gitignore

--- CreditCardEntry/src/com/devmarvel/creditcardentry/library/CardType.java
+++ CreditCardEntry/src/com/devmarvel/creditcardentry/library/CardType.java
@@ -33,7 +33,7 @@ public enum CardType implements Serializable {
     AMEX("American Express", R.drawable.amex, CardRegex.REGX_AMEX, CardRegex.REGX_AMEX_TYPE),
     DISCOVER("Discover", R.drawable.discover, CardRegex.REGX_DISCOVER, CardRegex.REGX_DISCOVER_TYPE),
     DINERS("DinersClub",R.drawable.diners_club,CardRegex.REGX_DINERS_CLUB,CardRegex.REGX_DINERS_CLUB_TYPE),
-    JCB("JCB",R.drawable.amex,CardRegex.REGX_JCB,CardRegex.REGX_JCB_TYPE),
+    JCB("JCB",R.drawable.jcb_payment_ico,CardRegex.REGX_JCB,CardRegex.REGX_JCB_TYPE),
     INVALID("Unknown", R.drawable.unknown_cc, null, null);
 
   /** name for humans */
