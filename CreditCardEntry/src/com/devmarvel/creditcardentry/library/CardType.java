d3364021125500ed1bfd2733e24f399a17ad839e
Ifiok Idiang <ifiok.idiang@gmail.com>
2017-10-27 22:31:29 +0100

    updated Mastercard type with new BIN range

--- CreditCardEntry/src/com/devmarvel/creditcardentry/library/CardType.java
+++ CreditCardEntry/src/com/devmarvel/creditcardentry/library/CardType.java
@@ -9,7 +9,7 @@ import java.io.Serializable;
 class CardRegex {
     // See: http://www.regular-expressions.info/creditcard.html
     static final String REGX_VISA = "^4[0-9]{15}?"; // VISA 16
-    static final String REGX_MC = "^5[1-5][0-9]{14}$"; // MC 16
+    static final String REGX_MC = "^(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}$"; // MC 16
     static final String REGX_AMEX = "^3[47][0-9]{13}$"; // AMEX 15
     static final String REGX_DISCOVER = "^6(?:011|5[0-9]{2})[0-9]{12}$"; // Discover 16
     static final String REGX_DINERS_CLUB = "^3(?:0[0-5]|[68][0-9])[0-9]{11}$"; // DinersClub 14
@@ -17,7 +17,7 @@ class CardRegex {
     static final String REGX_VERVE = "^(506099|5061[0-8][0-9]|50619[0-8])[0-9]{13}$"; // Interswitch Verve [Nigeria]
 
     static final String REGX_VISA_TYPE = "^4[0-9]{3}?"; // VISA 16
-    static final String REGX_MC_TYPE = "^5[1-5][0-9]{2}$"; // MC 16
+    static final String REGX_MC_TYPE = "^(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)$"; // MC 16
     static final String REGX_AMEX_TYPE = "^3[47][0-9]{2}$"; // AMEX 15
     static final String REGX_DISCOVER_TYPE = "^6(?:011|5[0-9]{2})$"; // Discover 16
     static final String REGX_DINERS_CLUB_TYPE = "^3(?:0[0-5]|[68][0-9])[0-9]$"; // DinersClub 14
